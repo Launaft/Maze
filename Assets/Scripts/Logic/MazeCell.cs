7ba48649e8bb77d30c34e579cce237485c92142e
Launaft <xgamergame8@gmail.com>
2024-10-08 23:48:54 +0700

    Initial commit

---
+++ Assets/Scripts/Logic/MazeCell.cs
@@ -0,0 +1,12 @@
+public class MazeCell
+{
+    public int X;
+    public int Y;
+
+    public bool North = true;
+    public bool South = true;
+    public bool East = true;
+    public bool West = true;
+
+    public bool Visited = false;
+}
